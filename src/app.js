866e0ef389cd42ed6c37f6abeb9c83f25f228e82
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-05-20 00:16:37 -0400

    feat(src): initial upload based on xstream-boilerplate
    This makes uses of https://github.com/TylorS/xstream-boilerplate

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import 'es6-shim' // suppport more browsers
+// External imports
+import Cycle from '@cycle/xstream-run'
+import {makeDOMDriver} from '@cycle/dom'
+import {makeRouterDriver, supportsHistory} from 'cyclic-router'
+import {createHistory, createHashHistory} from 'history'
+
+// Local imports
+import main from 'page/main'
+
+const history = supportsHistory()
+  ? [createHistory(), {capture: true}]
+  : [createHashHistory(), {capture: false}]
+
+const drivers =
+  {
+    DOM: makeDOMDriver('#app', {transposition: false}),
+    router: makeRouterDriver(...history)
+  }
+
+const dispose = Cycle.run(main, drivers)
+
+if (module.hot) {
+  module.hot.accept()
+  module.hot.dispose(() => {
+    dispose()
+  })
+}
